96da9b3eea086bee025a0d173521419d65920626
Varin6 <varinator@gmail.com>
2019-08-06 00:32:52 +0100

    Camera follow, basic movement, restart on collision

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform rocket;
+
+    public float distanceFromObject = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        Vector3 playerLastPosition = transform.position + rocket.position;
+
+        Vector3 temp = rocket.position;
+        temp.z = temp.z - 25;
+        temp.y = temp.y + 1;
+
+        transform.position = temp;
+
+
+        transform.LookAt(rocket, Vector3.up);
+        
+        //Vector3 lookOnObject = rocket.position - (rocket.up * 5);
+
+        //transform.forward = lookOnObject.normalized;
+
+
+        //Vector3 playerLastPosition = rocket.position - lookOnObject.normalized * distanceFromObject;
+        
+    }
+
+
+    
+
+
+
+}
+
--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,46 +6,88 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     private Rigidbody rigidbody;
+    private AudioSource audioSource;
+    [SerializeField] float rotationPower = 100f;
+    [SerializeField] float thrustPower = 100f;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        rigidbody.constraints = RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezePositionZ;
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+
+        Thrust();
+        Rotate();
+
     }
 
-    private void ProcessInput()
+    void OnCollisionEnter(Collision collision)
     {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                break;
+            case "Cunt":
+                Application.LoadLevel(Application.loadedLevel);
+                break;
+        }
+    }
+
+
+    private void Thrust()
+    {
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            audioSource.Stop();
+            rigidbody.freezeRotation = false;
+        }
 
 
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidbody.AddRelativeForce(Vector3.up);
+            float thrustThisFrame = thrustPower * Time.deltaTime;
+            rigidbody.freezeRotation = true;
+            rigidbody.AddRelativeForce(Vector3.up * thrustThisFrame);
+            
+            
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+
         }
+
+    }
+
+
+
+
+    private void Rotate()
+    {
+
 
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            float rotationThisFrame = rotationPower * Time.deltaTime;
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            float rotationThisFrame = rotationPower * Time.deltaTime;
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
 
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Rotate(Vector3.left);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            transform.Rotate(-Vector3.left);
-        }
+
     }
+
+
 }
